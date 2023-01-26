e946fb0ed793e9911351a2d086d7f14f93ca5a64
Chinemelu Nwosu <chinemelu@seamlesshr.com>
2023-01-26 07:49:28 +0100

    Complete setup of Lazy loading section

---
+++ lazyLoading/index.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const images = document.querySelectorAll('[data-src]')    
+
+
+})
--- stickyHeader/index.js
+++ stickyHeader/index.js
@@ -3,25 +3,4 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
     const topContainer = document.getElementById('top-container')
 
-    const options = {
-        // root: null,
-        // rootMargin: '0px 0px 0px 0px',
-    }
-    
-
-    const intersectionCallback = (entries) => {
-        entries.forEach((entry) => {
-          if (!entry.isIntersecting) {
-            header.classList.add('sticky')
-          } else {
-            header.classList.remove('sticky')
-          }
-        });
-    }
-
-    const observer = new IntersectionObserver(intersectionCallback, options);
-
-
-
-    observer.observe(topContainer)
 });
